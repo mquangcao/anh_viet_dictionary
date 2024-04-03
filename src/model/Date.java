6deebf989fd12b7c0f697c15814fe175e63aa164
mquangcao <119126554+mquangcao@users.noreply.github.com>
2024-04-03 10:36:32 +0700

    fix[historyUI] : add state search

---
+++ src/model/Date.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Date {
+    private String day;
+    private String month;
+    private String year;
+
+    public String getDay() {
+        return this.day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return this.month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return this.year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+}
